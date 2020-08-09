87bc5eb2d5c9e222d03e5d914154843c694e66c3
Camilo Carrasco Calbillan <carrascocamilo@gmail.com>
2020-08-09 11:12:35 -0400

    Update podOperator.py
    Add Pod Operator

--- podOperator.py
+++ podOperator.py
@@ -1,3 +1,4 @@
+#HERE DECLARE LIBS AND OPERATORS
 import airflow
 from airflow import DAG
 from airflow.operators.sensors import TimeDeltaSensor
@@ -14,3 +15,51 @@ import logging
 import os
 import sys
 import glob
+
+
+#HERE DECLARE DAG DEFINITION
+start = datetime.today() - timedelta(days=1)
+default_args = {
+    'owner': 'Udacity-DAG',
+    'start_date': start,
+    'email': 'carrascocamilo@gmail.com',
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+    }
+
+dag = DAG('00_DAG_UDACITY_POD_OPERATOR', default_args=default_args, schedule_interval="0 0 * * 1-5",tags=['GIT','POD OPERATOR'])
+
+
+#HERE DECLARE VOLUME AND MOUNT FROM AZURE PVC 
+volume_mounts  = [VolumeMount('airflow-files', mount_path='/data',sub_path=None, read_only=False)]
+volumes  = [Volume(name='airflow-files', configs={'persistentVolumeClaim': {'claimName': 'azfile-airflow-tmpfiles'}})]
+
+#HERE DECLARE POD OPERATOR
+PodOperatorExample = KubernetesPodOperator(
+    namespace='udacity-airflow',
+    image='name-cluster.azurecr.io/name-image-docker-registry:tag-image', #define Image for pod, source dockerHub or AzureRegistry
+    task_id='00_PodOperatorUdacity',
+    xcom_push=True,
+    xcom_all=True,
+    executor_config={
+        "KubernetesExecutor": {"request_memory": "5632Mi", "limit_memory": "5632Mi", "request_cpu": "3000m","limit_cpu": "3000m"} #define resouces from cluster
+                    },
+    pool='pool_udacity', #define pool task 
+    name='declare_PodOperatorUdacity', #define name is requerired for generate pod name 
+    priority_weight=10,
+    wait_for_downstream=True,
+    cmds=["bash", "-c"], #invoke bash inside container
+    arguments=["python /opt/process_input.py"], #run my script with files param csv 
+    env_vars={
+        'INPUT_PATH': '/data/input_csv_before_process.csv',
+        'OUTPUT_PATH': '/data/input_csv_after_process.csv'
+    },
+    volumes=volumes, #Use Volume
+    volume_mounts=volume_mounts, #mount Volume
+    is_delete_operator_pod=True, #delete pod after run
+    dag=dag)
+
+
+
